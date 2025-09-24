74c1335d79060fae9e3c61e98c74e583e9e47106
Ahmed Yusuf <ahmed.yusuf@edu.sait.ca>
2025-09-24 11:42:10 -0600

    Update iOS bundle ID and Google OAuth client IDs
    Set the iOS bundleIdentifier in app.json and update the androidClientId and webClientId for Google authentication in signin.tsx to use new credentials.

--- app/signin.tsx
+++ app/signin.tsx
@@ -16,8 +16,8 @@ export default function Signin(){
 
   const [request, response, promptAsync] = useAuthRequest({
     iosClientId: '324710914967-jjmjdq4iia0jrmsq8m6eluvius4h685j.apps.googleusercontent.com',
-    androidClientId: '324710914967-fjfamhom8glue4lq33vjgis423ur7ohp.apps.googleusercontent.com',
-    webClientId: '324710914967-sicu98bsg9v4i9qtjrgm8tlovg4obj77.apps.googleusercontent.com',
+    androidClientId: '324710914967-b4fferamcdihsql6ct9hkolt4778rtgm.apps.googleusercontent.com',
+    webClientId: '324710914967-1k6aeppu0ddffbris6v4k4o69jjaab02.apps.googleusercontent.com',
     redirectUri: "https://auth.expo.io/@ahmed100th/Future-Focus-Space",
   });
 
